2dbc871915050fa394162f94081a4f63dcb524d4
auraluvsu <ntreasure2015@gmail.com>
2025-05-24 21:53:02 +0100

    24/05/2025 || Batman

---
+++ nyx/piece.go
@@ -0,0 +1,63 @@
+package nyx
+
+import (
+	"fmt"
+)
+
+type Piece struct {
+	Type     pieceType // Each piece has a specific type assigned to it e.g. Knight
+	Colour   colour    //Playing for white or black
+	HasMoved bool      //Making sure turn based logic is working
+}
+
+type Move struct {
+	Fx, Fy   int  // Fx (From x coordinate), Fy (From y coordinate)
+	Tx, Ty   int  // Tx (To x coordinate), Ty (To y coordinate)
+	Promoted bool //Keeps track of pieces that have been promoted
+}
+
+type colour string
+type pieceType string
+
+const (
+	White colour = "White"
+	Black colour = "Black"
+)
+
+const ( // Piece Types
+	Pawn   pieceType = "Pawn"
+	Knight pieceType = "Knight"
+	Bishop pieceType = "Bishop"
+	Rook   pieceType = "Rook"
+	King   pieceType = "King"
+	Queen  pieceType = "Queen"
+)
+
+func makeBoard() [][]uint8 {
+	board := make([][]uint8, 8) // Outer Board
+	for i := range 8 {
+		board[i] = make([]uint8, 8) // Inner Board
+	}
+	return board
+}
+
+func chessToIndex(pos string) (x, y int, err error) {
+	if len(pos) != 2 {
+		return -1, -1, fmt.Errorf("Error! Invalid position")
+	}
+	file := pos[0] - 'a'
+	rank := pos[1] - '1'
+	if file < 0 || file >= 8 || rank < 0 || rank >= 8 {
+		return -1, -1, fmt.Errorf("Error: out of bounds position")
+	}
+	return int(file), 7 - int(rank), nil
+}
+
+func indexToChess(x, y int) (string, error) {
+	if x >= 8 || y >= 8 || x < 0 || y < 0 {
+		return "", fmt.Errorf("Error! Invalid index")
+	}
+	file := rune('a' + x)
+	rank := rune('8' - y)
+	return string([]rune{file, rank}), nil
+}
