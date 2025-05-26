11e8651356f0be8f9c202d52726ab16b18221b34
auraluvsu <ntreasure2015@gmail.com>
2025-05-26 08:05:11 +0100

    26/05/2025 || Finished basic piece movement

--- nyx/setup/movement.go
+++ nyx/setup/movement.go
@@ -4,6 +4,14 @@ import (
 	"math"
 )
 
+func colorCheck(mp *Piece, op [8][8]*Piece, toX, toY int) bool {
+	target := op[toX][toY]
+	if target != nil && target.Colour == mp.Colour {
+		return false
+	}
+	return true
+}
+
 func inBounds(x, y int) bool {
 	return x >= 0 && x < 8 && y >= 0 && y < 8
 }
@@ -18,8 +26,7 @@ func (p *Piece) IsValidKnightMove(fromX, fromY, toX, toY int, board [8][8]*Piece
 	absDx := int(math.Abs(float64(dx)))
 	absDy := int(math.Abs(float64(dy)))
 
-	target := board[toX][toY]
-	if target != nil && target.Colour == p.Colour {
+	if !colorCheck(p, board, toX, toY) {
 		return false
 	}
 	if (absDx == 2 && absDy == 1) || (absDx == 1 && absDy == 2) {
@@ -28,13 +35,75 @@ func (p *Piece) IsValidKnightMove(fromX, fromY, toX, toY int, board [8][8]*Piece
 	return false
 }
 
-func (p *Piece) IsValidRookMove(fromX, toX, fromY, toY int, board [8][8]*Piece) bool {
+func (p *Piece) IsValidRookMove(fromX, fromY, toX, toY int, board [8][8]*Piece) bool {
 	if !inBounds(toX, toY) {
 		return false
 	}
-	dx := toX - fromX
-	dy := toY - fromY
+	if !colorCheck(p, board, toX, toY) {
+		return false
+	}
+	if fromX != toX && fromY != toY {
+		return false
+	}
+	if fromY == toY {
+		for i := min(fromX, toX) + 1; i < max(fromX, toX); i++ {
+			if board[i][fromY] != nil {
+				return false
+			}
+		}
+	} else {
+		for i := min(fromY, toY) + 1; i < max(fromY, toY); i++ {
+			if board[fromX][i] != nil {
+				return false
+			}
+		}
+	}
+	return true
+}
 
-	absDx := int(math.Abs(float64(dx)))
-	absDy := int(math.Abs(float64(dy)))
+func (p *Piece) IsValidBishopMove(fromX, fromY, toX, toY int, board [8][8]*Piece) bool {
+	if !inBounds(toX, toY) {
+		return false
+	}
+	if !colorCheck(p, board, toX, toY) {
+		return false
+	}
+	if math.Abs(float64(toX-fromX)) != math.Abs(float64(toY-fromY)) {
+		return false
+	}
+	dx := 1
+	if toX < fromX {
+		dx = -1
+	}
+	dy := 1
+	if toY < fromY {
+		dy = -1
+	}
+	x, y := fromX+dx, fromY+dy
+	for x != toX && y != toY {
+		if board[x][y] != nil {
+			return false
+		}
+		x += dx
+		y += dy
+	}
+	return true
+}
+
+func (p *Piece) IsValidQueenMove(fromX, fromY, toX, toY int, board [8][8]*Piece) bool {
+	// Check if destination is in bounds
+	if !inBounds(toX, toY) {
+		return false
+	}
+	// Check if there is a piece on that destination and if its your piece
+	if !colorCheck(p, board, toX, toY) {
+		return false
+	}
+	// Check if your move is either a valid rook move or a valid bishop move
+	if p.IsValidRookMove(fromX, fromY, toX, toY, board) ||
+		p.IsValidBishopMove(fromX, fromY, toX, toY, board) {
+		return true
+	}
+	// If the move is valid for neither piece, return false so you can't move
+	return false
 }
