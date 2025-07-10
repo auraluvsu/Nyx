fe4ad1dda37eda364ccf3574f274bff34b08abc3
auraluvsu <ntreasure2015@gmail.com>
2025-07-11 00:40:50 +0100

    Working on zig

--- .zig/main.zig
+++ .zig/main.zig
@@ -1,5 +1,11 @@
+const std = @import("std");
 const print = @import("std").debug.print();
 
 pub fn main() void {
-    print("Hello world!\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    var mem = try allocator.alloc(u8, 100);
+    defer allocator.free(mem);
+    mem[0] = 42;
+    print("mem[0] = {}\n", .{mem[0]});
 }
