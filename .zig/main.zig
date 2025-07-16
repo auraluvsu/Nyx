86817bdae3eaa98eb24b5f02d2f38a99086ff41a
auraluvsu <ntreasure2015@gmail.com>
2025-07-16 11:41:06 +0100

    Learning comptime

--- .zig/main.zig
+++ .zig/main.zig
@@ -1,11 +1,10 @@
 const std = @import("std");
 const print = @import("std").debug.print();
+const assert = @import("std").debug.assert();
+const mem = @import("std").mem;
 
-pub fn main() void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    var mem = try allocator.alloc(u8, 100);
-    defer allocator.free(mem);
-    mem[0] = 42;
-    print("mem[0] = {}\n", .{mem[0]});
+const foo: [5]u8 = .{1, 2,3,4,5};
+const foor: [4]u8 = .{1,2,3,4};
+comptime {
+    assert(mem.eql(u8, &foo, &foor));
 }
